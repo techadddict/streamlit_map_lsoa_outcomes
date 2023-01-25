1e19326482b5f7af0c7fce300f795b96de5334ed
Anna Laws <a.laws2@exeter.ac.uk>
2023-01-25 14:41:18 +0000

    Basic test up and working

--- pages/2_Interactive_demo.py
+++ pages/2_Interactive_demo.py
@@ -9,15 +9,16 @@ done in functions stored in files named container_(something).py
 """
 # ----- Imports -----
 import streamlit as st
+from streamlit_folium import folium_static
+
+# Importing libraries
+import folium
+from folium import plugins
+import pandas as pd
+import json
 
 # Custom functions:
-from utilities.fixed_params import page_setup
-from utilities.inputs import \
-    write_text_from_file
-# Containers:
-import utilities.container_inputs
-import utilities.container_results
-import utilities.container_details
+from utilities_maps.fixed_params import page_setup
 
 
 # ###########################
@@ -26,44 +27,51 @@ import utilities.container_details
 page_setup()
 
 # Title:
-st.markdown('# Interactive demo')
-# Draw a blue information box:
-st.info(
-    ':information_source: ' +
-    'For acronym reference, see the introduction page.'
-    )
-# Intro text:
-write_text_from_file('pages/text_for_pages/2_Intro_for_demo.txt',
-                     head_lines_to_skip=2)
+st.markdown('# Folium map test')
+
+## Load data files
+# travel times
+df_travel_times = pd.read_csv("./data_maps/clinic_travel_times.csv")
+
+# clinic info
+df_clinics = pd.read_csv("./data_maps/clinic_locations.csv")
+
+# lsoa geojson
+with open('./data_maps/cornwall.geojson') as f:
+    geojson_cornwall = json.load(f)
+
+## Copy the LSOA11CD code to features.id within geojson
+for i in geojson_cornwall['features']:
+    i['id'] = i['properties']['LSOA11CD']
+
+# Create a map
+clinic_map = folium.Map(location=[50.435, -5.09426],
+                        zoom_start=9,
+                        tiles='cartodbpositron')
+# Add markers
+for (index, row) in df_clinics.iterrows():
+    pop_up_text = f"The postcode for {row.loc['Name']} " + \
+                    f"({row.loc['Clinic']}) is {row.loc['postcode']}"
+    folium.Marker(location=[row.loc['lat'], row.loc['long']], 
+                  popup=pop_up_text, 
+                  tooltip=row.loc['Name']).add_to(clinic_map)
+    
+# Add choropleth
+folium.Choropleth(geo_data=geojson_cornwall,
+                  name='choropleth',
+                  data=df_travel_times,
+                  columns=['LSOA11CD', 'travel_time_mins'],
+                  key_on='feature.id',
+                  fill_color='OrRd',
+                  fill_opacity=0.5,
+                  line_opacity=0.5,
+                  legend_name='travel_time_mins',
+                  highlight=True).add_to(clinic_map)
+
+# Generate map
+# clinic_map
+folium_static(clinic_map)
 
 
-# ###########################
-# ########## SETUP ##########
-# ###########################
-st.header('Setup')
-st.subheader('Inputs')
-
-# Draw the input selection boxes in this function:
-animals_df, animal, feature, row_value = utilities.container_inputs.main()
-
-
-# ###########################
-# ######### RESULTS #########
-# ###########################
-st.header('Results')
-# Draw a plot in this function:
-utilities.container_results.main(row_value, animal, feature)
-
-
-# ###########################
-# ######### DETAILS #########
-# ###########################
-st.write('-'*50)
-st.header('Details of the calculation')
-st.write('The following bits detail the calculation.')
-
-with st.expander('Some details'):
-    # Draw the equation in this function:
-    utilities.container_details.main(animal, feature, row_value)
 
 # ----- The end! -----
